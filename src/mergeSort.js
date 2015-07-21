58a2fa7385f5d6d35156fb787a7911921ffcbedd
Justin Tormey <jrtormey@gmail.com>
2015-07-21 00:38:45 -0400

    Added mergeSort solution

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -10,4 +10,27 @@ var mergeSort = function (array) {
 
 function merge (left, right) {
 	// Implement a helper function to merge elements here
+	var result = [],
+	i = 0,
+	j = 0;
+
+	// Compare elements in the left and right lists to each other
+	// Add the smallest element until one list is empty
+	while (i < left.length && j < right.length) {
+		if (left[i] < right[j]) {
+
+			result.push( left[i++]  );
+
+		} else {
+
+			result.push( right[j++] );
+
+		}
+	}
+
+	// Append leftover elements to the end of the resultant list
+	// We can assume that this new list will be in sorted order
+	result = result.concat( left.slice(i)  );
+	result = result.concat( right.slice(j) );
+	return result;
 };
