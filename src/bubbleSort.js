8fcbc9f8ba10440a88e56347bea432f559583cd8
Justin Tormey <jrtormey@gmail.com>
2015-07-21 00:13:02 -0400

    Added bubbleSort solution

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,3 +1,12 @@
 var bubbleSort = function (array) {
-	// Your solution here
+	for (var i = 0; i < array.length; i++) {
+		for (var j = array.length; j > 0; j--) {
+			if (array[j] < array[j - 1]) {
+				var tmp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = tmp;
+			}
+		}
+	}
+	return array;
 };
